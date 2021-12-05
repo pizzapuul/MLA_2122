cacb954bce9892e3478db26689a78cf9f8f9b530
pizzapuul <joost.ohrenberg@gmail.com>
2021-12-05 20:06:25 +0100

    minor changes

--- Aufgabe 2/preprocessing_data.py
+++ Aufgabe 2/preprocessing_data.py
@@ -8,7 +8,7 @@ import missingno as msn
 from shapely.geometry import Point, Polygon
 
 #Preparation for plotting data over europe
-fig, ax = plt.subplots(figsize=(8,6))
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(30,20))
 world = gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
 europe=world[world.continent=='Europe']
 #Remove Russia and Iceland from map of Europe
@@ -17,42 +17,50 @@ europe=europe[(europe.name!='Russia') & (europe.name!='Iceland')]
 polygon = Polygon([(-25,35), (40,35), (40,75),(-25,75)])
 #Clip polygon from the map of Europe
 europe=gpd.clip(europe, polygon)
-base = europe.plot(color='#3B3C6E', ax=ax)
-   
+europe.plot(color='#3B3C6E', ax=axes[0], alpha=0.8)
+europe.plot(color='#3B3C6E', ax=axes[1], alpha=0.8)
+# europe.plot(color='#3B3C6E', ax=axes[1,0], alpha=0.8)
+# europe.plot(color='#3B3C6E', ax=axes[1,1], alpha=0.8)
+cmap = mpl.colors.LinearSegmentedColormap.from_list("", ["green","yellow","red"])
+
+#change values to actual timestamps
+date_range = pd.date_range(start ='1-1-2020', end ='12-31-2020', freq ='24H')
+date_list = [str(d.date()) for d in date_range]
+day_list = []
+for i in range(1, 367):
+    day_list.append(str(i))
+
 for file in sorted(os.listdir('C:/Users/BIE/Desktop/Python/MLA/MLA_2122/data')):
-    data = pd.read_csv('data/'+file)
-    #msn.bar(data, color='darkolivegreen') #checking on missing values
-    data = pd.DataFrame.dropna(data) #deleting rows which contain NAN values 
-    #data = data[data['movement_state'].notna()] #deleting rows which contain NAN values in specific columns
-    #data=data.replace(to_replace=['parking', 'standing', 'moving'], value=[1, 2, 3]) #replacing strings with int
-    #data=data.replace(to_replace=['Leer', 'Beladen'], value=[0, 1]) #replacing strings with int
-    data['timestamp_measure_movement_state'] = data['timestamp_measure_movement_state'].str[:16] #removing unwanted characters
-    data['timestamp_index'] = data['timestamp_index'].str[:16] #removing unwanted characters
+    data = pd.read_csv('data/'+file, usecols = ['latitude',
+        	                                    'longitude',
+                                                'timestamp_transfer',
+                                                'timestamp_measure_position',
+                                                'signal_quality_satellite',
+                                                'signal_quality_hdop',
+                                                'determination_position',
+                                                'provider'
+                                                ], low_memory = True)
 
+    data = pd.DataFrame.dropna(data) #deleting rows which contain NAN values
+    data['timestamp_transfer'] = data['timestamp_transfer'].str[:16] #removing unwanted characters
+    data['timestamp_measure_position'] = data['timestamp_measure_position'].str[:16] #removing unwanted characters
 
-    #change values to actual timestamps
-    date_range = pd.date_range(start ='1-1-2020', end ='12-31-2020', freq ='24H')
-    date_list = [str(d.date()) for d in date_range]
-    day_list = []
-    for i in range(1, 367):
-        day_list.append(str(i))
+    #for timestamp_transfer
+    timestamp_transfer_date= data.timestamp_transfer.str[:2] #Just the Days of the Timestamp
+    timestamp_transfer_date = timestamp_transfer_date.replace(to_replace=day_list, value=date_list) #change Days to actual date e.g. 42 days -> 2020-02-11
+    timestamp_transfer_hours = data.timestamp_transfer.str[-8:] #Just the Time of the Timestamp
+    data.timestamp_transfer =timestamp_transfer_date +" "+timestamp_transfer_hours #Combine Days and Hours in one column
+    data['timestamp_transfer'] = pd.to_datetime(data.timestamp_transfer) #Convert to actual timestamps
 
-    #for timestamp_index
-    timestamp_index_date= data.timestamp_index.str[:2] #Just the Days of the Timestamp
-    timestamp_index_date = timestamp_index_date.replace(to_replace=day_list, value=date_list) #change Days to actual date e.g. 42 days -> 2020-02-11
-    timestamp_index_hours = data.timestamp_index.str[-8:] #Just the Time of the Timestamp
-    data.timestamp_index =timestamp_index_date +" "+timestamp_index_hours #Combine Days and Hours in one column
-    data['timestamp_index'] = pd.to_datetime(data.timestamp_index) #Convert to actual timestamps
-
-    #for timestamp_measure_movement_state
-    timestamp_measure_movement_state_date= data.timestamp_measure_movement_state.str[:2] #Just the Days of the Timestamp
-    timestamp_measure_movement_state_date = timestamp_measure_movement_state_date.replace(to_replace=day_list, value=date_list) #change Days to actual date e.g. 42 days -> 2020-02-11
-    timestamp_measure_movement_state_hours = data.timestamp_measure_movement_state.str[-8:] #Just the Time of the Timestamp
-    data.timestamp_measure_movement_state =timestamp_measure_movement_state_date +" "+timestamp_measure_movement_state_hours #Combine Days and Hours in one column
-    data['timestamp_measure_movement_state'] = pd.to_datetime(data.timestamp_measure_movement_state) #Convert to actual timestamps
+    #for timestamp_measure_position
+    timestamp_measure_position_date= data.timestamp_measure_position.str[:2] #Just the Days of the Timestamp
+    timestamp_measure_position_date = timestamp_measure_position_date.replace(to_replace=day_list, value=date_list) #change Days to actual date e.g. 42 days -> 2020-02-11
+    timestamp_measure_position_hours = data.timestamp_measure_position.str[-8:] #Just the Time of the Timestamp
+    data.timestamp_measure_position =timestamp_measure_position_date +" "+timestamp_measure_position_hours #Combine Days and Hours in one column
+    data['timestamp_measure_position'] = pd.to_datetime(data.timestamp_measure_position) #Convert to actual timestamps
 
     #add coloumn with delta timestamp in seconds
-    data['delta_timestamps'] = (data.timestamp_index - data.timestamp_measure_movement_state).dt.total_seconds()
+    data['delta_timestamps'] = (data.timestamp_transfer - data.timestamp_measure_position).dt.total_seconds()
 
     # Change the coordinates to geoPoints
     data['coordinates'] = data[['longitude', 'latitude']].values.tolist()
@@ -60,15 +68,42 @@ for file in sorted(os.listdir('C:/Users/BIE/Desktop/Python/MLA/MLA_2122/data')):
     data = gpd.GeoDataFrame(data, geometry='coordinates')
 
     #changing delta_timestamps from seconds to qualitative value
-    data['delta_timestamps'] = np.where(data['delta_timestamps'].between(0,60), 1, data['delta_timestamps'])
+    data['delta_timestamps'] = np.where(data['delta_timestamps'].between(-10000,60), 1, data['delta_timestamps'])
     data['delta_timestamps'] = np.where(data['delta_timestamps'].between(60,300), 2, data['delta_timestamps'])
     data['delta_timestamps'] = np.where(data['delta_timestamps'].between(300,900), 3, data['delta_timestamps'])
     data['delta_timestamps'] = np.where(data['delta_timestamps'].between(900,3600), 4, data['delta_timestamps'])
     data['delta_timestamps'] = np.where(data['delta_timestamps']>3600, 5, data['delta_timestamps'])
-    cmap = mpl.colors.LinearSegmentedColormap.from_list("", ["green","yellow","red"])
-    data.plot(ax=base, column='delta_timestamps', marker="*", markersize=1, cmap=cmap, legend=True)
 
-_ = ax.axis('off')
+    #Plot delta_timestamps on europe map
+    data.plot(ax=axes[0], column='delta_timestamps', marker="o", markersize=1, cmap=cmap, legend=True)
+    axes[0].set_title('delta_timestamps')
+    axes[0].yaxis.set_visible(False)
+    axes[0].xaxis.set_visible(False)
+
+    #Plot provider on europe map
+    data.plot(ax=axes[1], column='provider', marker="o", markersize=10, cmap='cool', legend=True, alpha=0.1)
+    axes[1].set_title('provider')
+    axes[1].yaxis.set_visible(False)
+    axes[1].xaxis.set_visible(False)
+
+for i in range(0, 40):
+    prov_i= data.loc[data['provider'] == i]
+    print(prov_i['delta_timestamps'].mean(), prov_i['delta_timestamps'].var())
+    
+data_heatmap = data[['signal_quality_satellite',
+                    'signal_quality_hdop',
+                    'provider',
+                    'delta_timestamps'
+                    ]].copy()
+   
+corr = data_heatmap.corr()
+print(corr)
 plt.show()
 
 
+
+
+#data = data[data['movement_state'].notna()] #deleting rows which contain NAN values in specific columns
+#data=data.replace(to_replace=['parking', 'standing', 'moving'], value=[1, 2, 3]) #replacing strings with int
+#data=data.replace(to_replace=['Leer', 'Beladen'], value=[0, 1]) #replacing strings with int
+#msn.bar(data, color='darkolivegreen') #checking on missing values
--- get_sample_csv.py
+++ get_sample_csv.py
@@ -1,10 +1,10 @@
-
 import pandas as pd
 
-def sample_csv(data):
+def sample_csv(path):
     n = 1000  # every 1000th line = 0.1% of the lines
-    df = pd.read_csv(data, header=0, skiprows=lambda i: i % n != 0)
+    df = pd.read_csv(path, header=0, skiprows=lambda i: i % n != 0)
     df.to_csv('sample_data.csv', encoding='utf-8', index=False)
 
     return None
 
+#sample_csv('C:/Users/BIE/Desktop/Python/MLA/MLA_2122/data/***.csv')
